da5c1396ba95cad1eb390db4f7c3d3dd7e9b3b8a
jerrriicccc <jericgiron05@gmail.com>
2026-02-16 13:49:40 +0800

    Initial commit - LeetCode solutions

---
+++ JavaScript/reverseBits.js
@@ -0,0 +1,16 @@
+var reverseBits = function (n) {
+  let result = 0;
+
+  for (let i = 0; i < 32; i++) {
+    result = (result << 1) | (n & 1);
+    n = n >>> 1;
+  }
+
+  return result >>> 0;
+};
+
+// Test cases
+console.log(reverseBits(43261596)); // Expected: 964176192
+console.log(reverseBits(4294967293)); // Expected: 3221225471
+console.log(reverseBits(0)); // Expected: 0
+console.log(reverseBits(1)); // Expected: 2147483648
